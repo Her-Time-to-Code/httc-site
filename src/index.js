473aa0bf0467f6f29c4f1eb7ccff5f4736851c7a
Katrina <katrina@hertimetocode.com>
2021-09-06 17:46:21 -0400

    store set up

--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './redux/sagas/index'
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware) );
+sagaMiddleware.run(rootSaga);
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-/
